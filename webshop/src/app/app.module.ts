fb757c3ddb814e7232434f9c2b6d23b08563386c
Mónika <gergye.monika@gmail.com>
2022-11-29 23:11:45 +0100

    Alap komponensek
    -dasboard-ban lehet lapozni
    -Service lekéri az adatokat
    -adatmodellek felvéve
    -product page listáz töröl
    -adatlistázó kompones első változata

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { HomeComponent } from './page/home/home.component';
 import { HeaderComponent } from './common/header/header.component';
+import { DataTableComponent } from './common/data-table/data-table.component';
+import { ProductComponent } from './page/product/product.component';
+import { CustomerComponent } from './page/customer/customer.component';
+import { OrderComponent } from './page/order/order.component';
+import { BillComponent } from './page/bill/bill.component';
+import { FilterPipe } from './common/data-table/pipe/filter.pipe';
+import { SorterPipe } from './common/data-table/pipe/sorter.pipe';
+import { HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    DataTableComponent,
+    ProductComponent,
+    CustomerComponent,
+    OrderComponent,
+    BillComponent,
+    FilterPipe,
+    SorterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
