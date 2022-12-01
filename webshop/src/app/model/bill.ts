1ad4d01df8451d598409b69160aeb0c5151e32d3
Joe <reszketo.robert@gmail.com>
2022-12-01 18:04:11 +0100

    nested classes

--- webshop/src/app/model/bill.ts
+++ webshop/src/app/model/bill.ts
@@ -1,7 +1,9 @@
+import { Order } from "./order";
+
 export class Bill {
-    [x:string]:any;
-    id: number= 0;
-    orderID: number= 0;
-    amount: number= 0;
+    [x: string]: any;
+    id: number = 0;
+    orderID: Order = new Order();
+    amount: number = 0;
     status: string = "new|paid";
 }
--- webshop/src/app/model/order.ts
+++ webshop/src/app/model/order.ts
@@ -1,8 +1,11 @@
+import { Customer } from "./customer";
+import { Product } from "./product";
+
 export class Order {
-    [x:string]:any;
-    id:  number = 0;
-    customerID: number = 0;
-    productID:  number = 0;
-    amount:  number = 0;
+    [x: string]: any;
+    id: number = 0;
+    customerID: Customer = new Customer();
+    productID: Product = new Product();
+    amount: number = 0;
     status: string = "new|shipped|paid";
 }
