ae622b97baca5bfcd72c7b334dee6d8c6a5d9493
Petra <pasztor.petra1998@gmail.com>
2022-12-04 16:01:19 +0100

    filter

--- webshop/src/app/common/data-table/pipe/filter.pipe.ts
+++ webshop/src/app/common/data-table/pipe/filter.pipe.ts
@@ -1,12 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
 })
-export class FilterPipe implements PipeTransform {
+export class FilterPipe<T extends { [x: string]: any }>
+  implements PipeTransform
+{
+  transform(list: T[], phrase: string = '', key: string = ''): T[] {
+    if (!Array.isArray(list) || !phrase) {
+      return list;
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    phrase = phrase.toLowerCase();
+
+    if (key !== '') {
+      return list.filter((item) => {
+        const keys = key.split('.');
+        let element = item;
+        for (let i = 0; i < keys.length; i += 1) {
+          element = element[keys[i]];
+        }
+
+        if (typeof element === 'number') {
+          return Number(phrase) === element;
+        }
+        return ('' + element).toLowerCase().includes(phrase);
+      });
+    }
+
+    return list.filter((item) =>
+      Object.values(item).join(' ').toLowerCase().includes(phrase)
+    );
   }
-
 }
--- webshop/src/app/page/product/product.component.ts
+++ webshop/src/app/page/product/product.component.ts
@@ -7,31 +7,30 @@ import { DataService } from 'src/app/service/data.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-  
   list$: Observable<Product[]> = this.dataService.getAll('product');
 
   columns = this.config.productTableColumns;
 
+
   constructor(
-    private dataService:  DataService,
-    private config: ConfigService,
-  ) { }
+    private dataService: DataService,
+    private config: ConfigService
+  ) {}
 
   ngOnInit(): void {
     //this.productList$.subscribe(data=>{console.log(data)})
   }
 
-  onSelect( product:  Product): void {
-    console.log( product);
-   
+  onSelect(product: Product): void {
+    console.log(product);
   }
 
-  onDelete(product:  Product): void {
-    this.dataService.delete( product.id, 'product').subscribe(
-      () => this.list$ = this.dataService.getAll('product'),
-    );
+  onDelete(product: Product): void {
+    this.dataService
+      .delete(product.id, 'product')
+      .subscribe(() => (this.list$ = this.dataService.getAll('product')));
   }
 }
