3b4343facff91d492fa47086d461075a21b19858
Petra <pasztor.petra1998@gmail.com>
2022-12-07 13:20:56 +0100

    sum

--- webshop/src/app/common/data-table/pipe/table-col-sum.pipe.ts
+++ webshop/src/app/common/data-table/pipe/table-col-sum.pipe.ts
@@ -1,21 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'tableColSum'
+  name: 'tableColSum',
 })
-
-export class TableColSumPipe <T extends {[x: string]:any}>implements PipeTransform {
-
-  transform(rows: T[], colKey: string=''): string {
-    if(!Array.isArray(rows) || !colKey){
+export class TableColSumPipe<T extends { [x: string]: any }>
+  implements PipeTransform
+{
+  transform(rows: T[], colKey: string = ''): string {
+    if (!Array.isArray(rows) || !colKey) {
       return '';
     }
     let output = 0;
 
-    rows.forEach(row => {
-      output += Number(row[colKey]);  
-    })
-    return '' + output;
+    rows.forEach((row) => {
+      output += Number(row[colKey]);
+    });
+    return '' + output + ' $';
   }
-
 }
